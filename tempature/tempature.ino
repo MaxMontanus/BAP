4eb139acdd9ef103bdda53f12ab620023da49abb
MaxMontanus <maxmontanus1@gmail.com>
2022-03-31 12:03:07 +0200

    led functie

--- tempature/tempature.ino
+++ tempature/tempature.ino
@@ -1,70 +1,37 @@
-/*  
+```/*  
  *   Mediacollege Duurzaam huis BO
- *   Les 4
+ *   Les 3
  *   Connectie met internet  
  *   Webserver
- *   DHT11 temperature + humidity sensor
+ *   Leds On Off
 */
-
-
-//        initialise temperature humidity sensor DHT11
-#include "DHT.h"
-#define DHTTYPE DHT11       // DHT 11 sensor
-uint8_t DHTPin = D7;        // DHT Sensor data input
-DHT dht(DHTPin, DHTTYPE);   // Initialize DHT sensor.    
-float Temperature;          // temperature
-float Humidity;             // humidity
-float HeatIndex;            // Heatindex
- 
-//      web server
+// web server
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <ESP8266WebServer.h>
-ESP8266WebServer server(80);
+ESP8266WebServer server(80); // maak een instantie van de webserver op poort 80
+
+//LED pins
+uint8_t Led1 = D3; //LED indicator 
+uint8_t Led2 = D5; //LED indicator
+uint8_t Led3 = D6; //LED indicator
+
+bool led1Status = false; //toggle led on/off
+bool led2Status = false; //toggle led on/off
+bool led3Status = false; //toggle led on/off
 
 // your data
-String studentName = "Max student nummer 32044";
+String studentName = "Faissal 33278"; // jouw student nummer + naam
 
 const char* ssid = "Medialab";
 const char* password = "Mediacollege2";
 
-void readDHT11(){
-  /* 
-   *  read the sensor
-   *  sampling rate DHT11 is max. 1Hz 
-   *  the DHT11 is known for erroneous readings
-   */
-    float temperature =  round(dht.readTemperature()*10)/10; 
-    float humidity = round(dht.readHumidity()*10)/10;
-    // Compute heat index in Celsius (isFahrenheit = false)
-    float heatindex = round(dht.computeHeatIndex( Temperature, Humidity, false)*10)/10; 
-        
-    if(isnan(temperature) || isnan(humidity) || isnan(heatindex)){
-        // sensor error
-        Serial.println("DHT11 sensor error");
-    }
-    else{
-        // the DHT11 readings look ok 
-        Temperature = temperature;
-        Humidity =  humidity ;
-        HeatIndex = heatindex;
-        // show in Serial Monitor
-        Serial.print("Temp. ");
-        Serial.print(Temperature);
-        Serial.print("C. Humidity  ");
-        Serial.print(humidity);
-        Serial.print("% Heatindex ");
-        Serial.println(heatindex);
-      }
- } 
-
 void wifiConnect(){
-  WiFi.begin(ssid, password);
+  WiFi.begin(ssid, password); // connect to wifi ssid with password 
   while (WiFi.status() != WL_CONNECTED) {
-      delay(1000);
+      delay(1000); // wait 1 sec
       Serial.print("Connecting..");
       }
-      
   //if connected
   Serial.print("\r\nConnected: SSID: "); 
   Serial.print(ssid);
@@ -74,6 +41,86 @@ void wifiConnect(){
   server.begin();
   Serial.println("HTTP server started");
  }
+
+
+void handleRoot() {
+ server.send(200, "text/html", "<h3>Groetjesvan DuurzaamHuis! mijnnaam is: " +  studentName+  
+"<h3> <form action=\"/led1\" method=\"GET\"><input type=\"submit\" value=\"Toggle Led1\"></form> " + 
+"<form action=\"/led2\" method=\"GET\"><input type=\"submit\" value=\"Toggle Led2\"></form> " +
+"<form action=\"/led3\" method=\"GET\"><input type=\"submit\" value=\"Toggle Led3\"></form> "
+  );    
+}
+
+void handleNotFound(){
+  String message = "File Not Found\n\n";
+  server.send(404, "text/plain", message);
+}
+
+void setup(){
+  pinMode(Led1, OUTPUT);     // Initialize the pin as an output
+  pinMode(Led2, OUTPUT);     // Initialize the pin as an output
+  pinMode(Led3, OUTPUT);     // Initialize the pin as an output
+ 
+  Serial.begin(115200);      //  Start de serial monitor
+
+/*
+ * Webserver functionaliteit
+ *  server.on("/"
+ *  server.on("/easter_egg"
+ *  server.on("/led1"
+ *  server.on("/led2"
+ *  server.on("/led3"
+ *  server.onNotFound
+ */
+  server.on("/", handleRoot);
+
+  server.on("/easter_egg", [](){
+        server.send(200, "text/plain", "hoera dit werkt");
+    });
+
+  server.on("/led1", [](){
+       if(!led1Status && !digitalRead(Led1)){
+          server.send(200, "text/plain", "led1 on");
+          digitalWrite(Led1, HIGH); // led 1 on
+          led1Status = true;
+        }
+        else{
+          server.send(200, "text/plain", "led1 off");
+          digitalWrite(Led1, LOW); // led 1 on
+          led1Status = false;
+          }
+    });
+  server.on("/led2", [](){
+       if(!led2Status && !digitalRead(Led2)){
+          server.send(200, "text/plain", "led2 on");
+          digitalWrite(Led2, HIGH); // led 2 on
+          led2Status = true;
+        }
+        else{
+          server.send(200, "text/plain", "led2 off");
+          digitalWrite(Led2, LOW); // led 2 on
+          led2Status = false;
+          }
+    });
+  server.on("/led3", [](){
+       if(!led3Status && !digitalRead(Led3)){
+          server.send(200, "text/plain", "led3 on");
+          digitalWrite(Led3, HIGH); // led 3 on
+          led3Status = true;
+        }
+        else{
+          server.send(200, "text/plain", "led3 off");
+          digitalWrite(Led3, LOW); // led 3 on
+          led3Status = false;
+          }
+    });
+  server.onNotFound(handleNotFound);
+}
+
+void loop(){
+  if (WiFi.status() != WL_CONNECTED) wifiConnect();// reconnect Wifi if necessary 
+  server.handleClient();
+} ```
 
 void handleRoot() {
    server.send(200, "text/html", "<h3>Duurzaam Huis DHT11 sensor. <br> Mijn naam is: " 
