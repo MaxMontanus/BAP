171aba161ec09a8f26dc4e7e4a64fb34d1ec9755
MaxMontanus <32044@ma-web.nl>
2022-04-07 09:38:16 +0200

    push

--- tempature/tempature.ino
+++ tempature/tempature.ino
@@ -21,7 +21,7 @@ bool led2Status = false; //toggle led on/off
 bool led3Status = false; //toggle led on/off
 
 // your data
-String studentName = "Faissal 33278"; // jouw student nummer + naam
+String studentName = "Max 32044"; // jouw student nummer + naam
 
 const char* ssid = "Medialab";
 const char* password = "Mediacollege2";
